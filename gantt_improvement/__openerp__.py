1cdbbd66b60bb4edfa6108851b7566abd999af68
Stéphane Codazzi <stephane.codazzi@gmail.com>
2016-08-17 19:49:15 +0200

    Change version number

--- gantt_improvement/__openerp__.py
+++ gantt_improvement/__openerp__.py
@@ -18,7 +18,7 @@ TeMPO - Gantt Improvement
  Support: https://github.com/stephane-/odoo_addons/issues
 
     """,
-    'version': '1.6',
+    'version': '1.7',
     'depends': ['web', 'web_gantt'],
     'js': [
         'static/src/js/gantt.js',
